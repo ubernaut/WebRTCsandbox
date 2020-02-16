951628f88962ee209231cc057ce72d63b5d96734
ubernaut <collin.schroeder@gmail.com>
2020-02-16 11:14:57 -0900

    added client skeleton

--- server/app.js
+++ server/app.js
@@ -1,13 +1,26 @@
 import express from "express";
 import appconfig from "./appconfig.js"
+import user from "./user.js"
 const app = express();
 
 console.log(appconfig)
 
 const users = []
 
-app.listen(appconfig.port, () => console.log(`listening at http://localhost:${appconfig.port}`));
+users.push(user("default", "1.2.3.4"));
+
+app.listen(
+  appconfig.port,
+  () => console.log(`listening at http://localhost:${appconfig.port}`));
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.post("/register", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/list", (req, res) => {
+  res.send(users);
 });
---
+++ server/user.js
@@ -0,0 +1,9 @@
+const user = (name, ip)=>{
+  let user ={};
+  user.name=name;
+  user.ip = ip;
+  return user;
+}
+
+
+export default user;
